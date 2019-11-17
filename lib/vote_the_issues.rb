67eb120bf2c80c071ebc21fd43646ced5f107dfa
github username <ortufte@gmail.com>
2019-11-17 17:27:45 -0600

    first commit

---
+++ lib/vote_the_issues.rb
@@ -0,0 +1,6 @@
+require "vote_the_issues/version"
+
+module VoteTheIssues
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/vote_the_issues/version.rb
@@ -0,0 +1,3 @@
+module VoteTheIssues
+  VERSION = "0.1.0"
+end
