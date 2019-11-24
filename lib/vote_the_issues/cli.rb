1a467178209e713b69b8e6683397e9eb0829b412
ortufte <ortufte@gmail.com>
2019-11-24 11:06:16 -0600

    cli working with hard coded data

--- lib/vote_the_issues/cli.rb
+++ lib/vote_the_issues/cli.rb
@@ -1,19 +1,19 @@
-class VoteTheIssues::CLI #namespacing 
+class VoteTheIssues::CLI
    
     def call
         puts "\nWelcome to Vote the Issues!\n"
-        puts "\nIf you would like to see a specific candidate's stance on a particular issue, enter 'candidate':\n"
+        puts "\nIf you would like to see a specific candidate's stance on a particular issue, enter 'candidates':\n"
         puts "\nOR\n"
-        puts "\nIf you would like to see how the the candidate's views compare on a particular issue, enter 'issues':\n"
+        puts "\nIf you would like to see how all the candidate's views compare on a particular issue, enter 'issues':\n"
         follow_user_preference
     end
 
     def follow_user_preference
         user_preference = gets.strip
-        if user_preference == 'candidate' 
+        if user_preference == 'candidates' 
             get_candidates
             view_candidates
-            get_candidate_issues
+            get_issues
             view_candidate_issues
             get_candidate_viewpoint
         elsif user_preference == 'issues'
@@ -21,7 +21,7 @@ class VoteTheIssues::CLI #namespacing
             view_issues
             view_issue
         elsif 
-            user_prefrence == 'exit'
+            user_preference == 'exit'
             exit
         else
             invalid_entry
@@ -38,78 +38,81 @@ class VoteTheIssues::CLI #namespacing
         puts puts "\nInvalid entry, please try again.\n"
     end
 
-#####################################################################################################################
-
     def get_candidates
-        @candidates = ["a", "b", "c"] #will be scraped
+        @candidates = [1, 2, 3, 4]
     end
 
     def view_candidates
         puts "\nPlease select a candidate by number:\n"
-        @candidates.each.with_index(1) {|candidate, index| puts "#{index}: #{candidate}"}
+        @candidates.each_with_index {|candidate, index| puts "#{index+1}: #{candidate}"}
     end
 
-    def get_candidate_issues
-        @candidate_issues = ["x", "y", "z"] #will be scraped
-    end
-
-    def valid_input(array)
-        user_input = gets.strip.to_i
-        user_input <= array.length && user_input > 0 
+    def get_issues
+        @issues = [1, 2, 3, 4]
     end
 
     def view_candidate_issues
-        if valid_input(@candidates)
-            puts "\nPlease select an issue by number:\n"
-        #VoteTheIssues::Candidate.all.each.with_index(1) do |candidate|
-        #@candidate_issues.each.with_index(1) {|issue, index| puts "#{index}: #{issue}"}
-        else 
+        until valid_input(@candidates)
             invalid_entry
-            view_candidate_issues
+            view_candidates
         end
-    end
-
-    def get_candidate_viewpoint
-        if valid_input(@candidate_issues) 
-            candidate_viewpoint 
-        else 
-            invalid_entry
-            view_candidate_issues
-        end
-        
-    end
-
-
-    def candidate_viewpoint
-        "test" #will be scraped
-    end
-
-############################################################################################
-    
-
-    def get_issues
-        @issues = ["1", "2", "3"] #will be scraped
+            view_issues
     end
 
     def view_issues
         puts "\nPlease select an issue by number:\n"
         @issues.each.with_index(1) {|issue, index| puts "#{index}: #{issue}"}
     end
-
-    def view_issue
-        if valid_input(@issues)
-            issue_viewpoints
-        else
+    
+    def get_candidate_viewpoint
+        until valid_input(@issues) 
             invalid_entry
             view_issues
         end
+        puts "candidate viewpoint here"
+
+        puts "\n To see another candidate's stance on a particular issue, enter 'candidates':\n"
+        puts "\n To see where all the candidates stand on a particular issue, enter 'issues':\n"
+        puts "\n Enter 'exit' to exit:\n"
+
+        follow_user_preference
+
+            #Candidate.issues each do |candidate_issue|
+                #if @issues[user_input-1] == candidate_issue
+                    #puts "{#Candidate.issues[user_input-1]}"
+                #else
+                    #puts "This candidate has no offical view on this issue yet."
+                #end
+    end
+
+    def view_issue
+        until valid_input(@issues)
+            invalid_entry
+            view_issues
+        end
+        issue_viewpoints
     end
 
     def issue_viewpoints
-        #VoteTheIssues::Issue.all.each.with_index(1) do |issue|
-        #   puts issue.name
-        #   puts issue.viewpoints
-        #puts "The following candidates share this viewpoint"
-        #puts candidates who share that viewpoint
+        #puts "Here are where all candidates stand on this #{issue}:"
+
+        #VoteTheIssues::Candidate.all.each do |candidate|
+            #puts "#{Candidate.name}:"
+            #Candidate.issues each do |candidate_issue|
+            #if @issues[user_input-1] == candidate_issue
+                #puts "{#Candidate.issues[user_input-1]}"
+            #else
+                #puts "This candidate has no offical view on this issue yet."
+            #end
+        #end
+        puts "issue viewpoints"
+
+        puts "\n To select another issue, enter 'issues':\n"
+        puts "\n To view a specific candidate's view on a specific issue, enter 'candidates':\n"
+        puts "\n Enter 'exit' to exit:\n"
+
+        follow_user_preference
+
     end
+
 end
